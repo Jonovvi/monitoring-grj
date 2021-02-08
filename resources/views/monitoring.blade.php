02f60eb89282330dfe321b691b0915569528a04c
FebriTahta <febrirtah@gmail.com>
2021-02-08 18:12:15 +0700

    y

--- resources/views/monitoring.blade.php
+++ resources/views/monitoring.blade.php
@@ -25,7 +25,7 @@
                   <p></p>
                   <p id="waktu"></p>
                   <p id="jam"></p>
-                  <input type="text" name="hm" id="hm" value="{{Request::get('a')}}">
+                  <input type="text" name="hm" id="hm" value="{{Request::get('x')}}">
               </div>
         </div>
       </section>
